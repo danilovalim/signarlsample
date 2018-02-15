b98c65b464284cfb2dec6d6232731bc01ddf1cf3
danilowecontent <danilo@wecontent.com.br>
2018-02-15 10:55:53 -0200

    first commit

---
+++ SignalR.Learning/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using SignalR.Learning;
+
+
+namespace SignalR.Learning.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void BroadcastMessage(string message)
+        {
+            Clients.All.writeMessage(message);
+        }
+    }
+}
---
+++ SignalR.Learning/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using SignalR.Learning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace SignalR.Learning
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
